16bd9096b0ff1ab00c173f8336759d33ae2aac82
Craig Simmons <craig.a.simmons@gmail.com>
2019-04-14 20:55:21 -0700

    quick format fix

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -40,25 +40,25 @@ $(document).ready(function() {
 
     /* Global variables */
 
-    var destination = "";
-    var name = "";
-    var start = "";
-    var frequency = "";
-    var trainNameStr = "";
-    var trainDestinationStr = "";
-    var startTimeStr = "";
-    var frequencyRateStr = "";
-    var dateAdded = "";
+    var destination;
+    var name;
+    var start;
+    var frequency;
+    var trainNameStr;
+    var trainDestinationStr;
+    var startTimeStr;
+    var frequencyRateStr;
+    var dateAdded;
     var nextArrival;
     var minsAway;
-    // Event handler for adding new trains
 
+    // Event handler for adding new trains
     $(document).on("submit", "#target-form", function(event) {
         event.preventDefault();
         addNewTrain();
     });
 
-    // watches Firebase - when new child object added, get data
+    // Event hndler for Firebase - when new child object added, get data
     database.ref().on('child_added', function(childSnapshot) {
         trainNameStr = childSnapshot.val().name;
         trainDestinationStr = childSnapshot.val().destination;
@@ -125,55 +125,12 @@ $(document).ready(function() {
 
     function timeCalc() {
         var convertedTime = moment(startTimeStr, "HH:mm").subtract(1, "years");
-        console.log(convertedTime);
         var currentTime = moment();
         var diffTime = moment().diff(moment(convertedTime), "minutes");
-        console.log("Differennce in time: " + diffTime);
-
         var timeModulo = diffTime % frequencyRateStr;
-        console.log(timeModulo);
-
         minsAway = frequencyRateStr - timeModulo;
-        console.log("Minutes until train: " + minsAway);
-
         nextArrival = moment().add(minsAway, "minutes");
         nextArrival = moment(nextArrival).format("HH:mm");
     }
-
-
-
-    /*
-        let currentTime = moment();
-        let convertedTime = moment(startTimeStr, "HH:mm")
-            // .subtract(1, "years");
-        console.log(currentTime);
-        console.log(convertedTime);
-        //Difference between the times
-        let diffTime = moment().diff(moment(convertedTime), "minutes");
-        console.log("Diff time: " + diffTime);
-        //Time apart
-        frequencyRateConverted = frequencyRateStr * 60000;
-        let timeModulo = diffTime % frequencyRateStr;
-        console.log(timeModulo);
-        //Minutes Until Train
-        while (currentTime > (startTimeStr + frequencyRateConverted)) {
-            startTimeStr = startTimeStr + frequencyRateConverted;
-            minsAway = startTimeStr - currentTime;
-        }
-
-        //minsAway = frequencyRateStr - timeModulo;
-        console.log("Mins until next: " + minsAway);
-        //Next arrival
-        nextArrival = moment().add(minsAway, "minutes");
-        console.log("Arrival: " + moment(nextArrival).format("HH:mm"));
-        nextArrival = moment(nextArrival).format("HH:mm");
-    }
-*/
-    /*
-        let train =firstDeparture
-while currentTime > train +freq
-train = train + freq
-time remaining = train - currenttime
-    */
 
 });
